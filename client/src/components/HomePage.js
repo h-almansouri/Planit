de466535fc3de89cc8f7da9e4e1c2affc1dffb24
Hisham Almansouri <hisham.almansouri1998@gmail.com>
2022-01-03 12:15:46 -0800

    initial home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,8 +3,22 @@ function HomePage(){
 
 
     return(
-        <div>
-            hi
+        <div className="home-div">
+            <div className="home-nav">
+                <span>Calender</span>
+                <button>Logout</button>
+            </div>
+            <div className="home-prof">
+                <span className="prof-pic">Prof Pic</span>
+                <h2>Welcome Username!</h2>
+            </div>
+            <div className="home-servers-list">
+                <span className="home-server" >Serv1</span>
+                <span className="home-server">Serv2</span>
+                <span className="home-server">Serv3</span>
+                <span className="home-server">Serv4</span>
+                <span className="home-server">+</span>
+            </div>
         </div>
     )
 }
