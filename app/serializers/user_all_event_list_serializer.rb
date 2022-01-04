c9a114f61147b95365c7ebfdaa8974e008b7493e
Michael Neis <michael.neis12@gmail.com>
2022-01-04 10:53:43 -0600

    initial calendar backend setup

--- app/serializers/user_all_event_list_serializer.rb
+++ app/serializers/user_all_event_list_serializer.rb
@@ -1,14 +1,15 @@
-class UserGroupEventListSerializer < ActiveModel::Serializer
-  attributes :list_group_events
+class UserAllEventListSerializer < ActiveModel::Serializer
+  attributes :list_all_events
 
-  def list_group_events
+  def list_all_events
       joined_events = []
       object.group_joins.each{|g| g.group_events.each{|e| joined_events << e}}
 
       admin_events = []
       object.group_admins.each{|g| g.group_events.each{|e| joined_events << e}}
 
-      all_events = joined_events + admin_events
+
+      all_events = joined_events + admin_events + object.personal_events
       all_events
   end
 end
