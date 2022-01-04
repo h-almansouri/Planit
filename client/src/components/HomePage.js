93efd4bea1a92ac43155254c4719cf3d72c844da
Michael Neis <michael.neis12@gmail.com>
2022-01-04 10:55:48 -0600

    calander rework

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -19,11 +19,28 @@ function HomePage({setCurrentUser}){
     }
 
     return(
-        <div>
-            homepage
-            <button onClick={handleLogout}>logout</button>
-            <GroupCircle array={allGroups} />
-        </div>
+        <div className="home-div">
+            <div className="home-nav">
+                <span>Calender</span>
+                <button>Logout</button>
+            </div>
+            <div className="home-prof">
+                <span className="prof-pic">Prof Pic</span>
+                <h2>Welcome Username!</h2>
+            </div>
+            <div className="home-servers-list">
+                <span className="home-server" >Serv1</span>
+                <span className="home-server">Serv2</span>
+                <span className="home-server">Serv3</span>
+                <span className="home-server">Serv4</span>
+                <span className="home-server">+</span>
+            </div>
+            <div>
+              homepage
+              <button onClick={handleLogout}>logout</button>
+              <GroupCircle array={allGroups} />
+            </div>
+      </div>
     )
 }
 
