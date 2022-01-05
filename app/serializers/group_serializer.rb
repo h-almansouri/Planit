66fe1672ef1648de3e0824fec661907094d8ffb0
Hisham Almansouri <hisham.almansouri1998@gmail.com>
2022-01-05 11:25:37 -0800

    Added initial group page

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,3 +1,6 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :group_picture, :admin_users, :joined_users
+  attributes :id, :name, :group_picture, :all_users
+  def all_users
+    new_array = object.admin_users + object.joined_users
+  end
 end
--- client/src/components/LoggedIn.js
+++ client/src/components/LoggedIn.js
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from './HomePage'
 import DisplayCalendar from './DisplayCalendar'
+import { useState } from "react";
+import GroupPage from "./GroupPage";
 
 
 function LoggedIn({ setCurrentUser, currentUser }){
-
+    const [groupId, setGroupId] = useState()
 
     return(
         <Routes>
-            <Route path='/' element={<HomePage setCurrentUser={setCurrentUser} currentUser={currentUser}/>} />
+            <Route path='/' element={<HomePage setCurrentUser={setCurrentUser} currentUser={currentUser} setGroupId={setGroupId}/>} />
             <Route path='/calendar' element={<DisplayCalendar currentUser={currentUser}/>}/>
+            <Route path='/group' element={<GroupPage groupId={groupId}/>}/>
         </Routes>
     )
 }
