3da9c7575a5e4e54dc0a0fe344fe2e2a87550d4b
Hisham Almansouri <hisham.almansouri1998@gmail.com>
2022-01-07 08:29:25 -0800

    removes user from groups when profile is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :personal_events
+    has_many :personal_events, dependent: :delete_all
     has_many :messages
-    has_many :joined_groups
-    has_many :admin_groups
+    has_many :joined_groups, dependent: :delete_all
+    has_many :admin_groups, dependent: :delete_all
     has_many :group_messages, through: :messages, source: :group
     has_many :group_joins, through: :joined_groups, source: :group
     has_many :group_admins, through: :admin_groups, source: :group
